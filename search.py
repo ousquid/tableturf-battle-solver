bb09f9befa78a2cedd9b1c1561756008eba4e9af
Zonkyy <zonk.yy30@gmail.com>
2022-11-09 21:38:30 +0900

    black によるフォーマット
    Co-authored-by: koiking213 <koiking213@gmail.com>
    Co-authored-by: MKDir43 <MKDir43@users.noreply.github.com>

--- search.py
+++ search.py
@@ -4,8 +4,10 @@ import glob
 import numpy as np
 import os
 
+
 class Point:
     pass
+
 
 class Rotation(Enum):
     TOP = 0
@@ -13,8 +15,16 @@ class Rotation(Enum):
     BOTTOM = 2
     LEFT = 3
 
+
 class Card:
-    def __init__(self, number: int, name: str, pattern: np.ndarray, ink_spaces: int, sp_attack_cost: int):
+    def __init__(
+        self,
+        number: int,
+        name: str,
+        pattern: np.ndarray,
+        ink_spaces: int,
+        sp_attack_cost: int,
+    ):
         self.number = number
         self.name = name
         self.pattern = pattern
@@ -25,25 +35,26 @@ class Card:
     def load_text(path: str):
         with open(path, "r") as f:
             lines = f.readlines()
-            
-        max_width = max([len(l.rstrip()) for l in lines])
+
+        max_width = max([len(line.rstrip()) for line in lines])
         max_height = len(lines)
         pattern = np.zeros((max_height, max_width)).astype(np.uint8)
-        
+
         for y, line in enumerate(lines):
             for x, p in enumerate(line):
                 if p == "o" or p == "x":
                     pattern[y, x] = 1
-        
+
         number = int(os.path.splitext(os.path.basename(path))[0])
         name = "Kojake"
         ink_spaces = 1
         sp_attack_cost = 1
         return Card(number, name, pattern, ink_spaces, sp_attack_cost)
-        
+
     @staticmethod
     def load_dir(path: str):
         return [Card.load_text(f) for f in glob.glob(path)]
+
 
 class Placement:
     def __init__(self, card: Card, point: Point, rotation: Rotation):
@@ -51,11 +62,13 @@ class Placement:
         self.point = point
         self.rotation = rotation
 
+
 class Solver:
     def search() -> Tuple[List[Card], List[Point]]:
         pass
 
-class Map():
+
+class Stage:
     def __init__(self, number: int, name: str, pattern: np.ndarray):
         self.number = number
         self.name = name
@@ -65,63 +78,66 @@ class Map():
     def load_text(path: str):
         with open(path, "r") as f:
             lines = f.readlines()
-            
-        max_width = max([len(l.rstrip()) for l in lines])
+
+        max_width = max([len(line.rstrip()) for line in lines])
         max_height = len(lines)
         pattern = np.zeros((max_height, max_width)).astype(np.uint8)
-        
+
         for y, line in enumerate(lines):
             for x, p in enumerate(line):
                 if p == "x":
                     pattern[y, x] = 1
-    
-        number = int(os.path.splitext(os.path.basename(path))[0])
-        name = "StraightStreet" 
-        return Map(number, name, pattern)
 
-def fill_eval(map: Map) -> int:
+        number = int(os.path.splitext(os.path.basename(path))[0])
+        name = "StraightStreet"
+        return Stage(number, name, pattern)
+
+
+def fill_eval(stage: Stage) -> int:
     pass
 
-def can_be_put(map: Map, card: Card) -> bool:
+
+def can_be_put(stage: Stage, card: Card) -> bool:
     # マップからはみ出ていないか
     # 他のカードと重ならないか
     pass
+
 
 def rotate() -> Card:
     pass
 
 
 if __name__ == "__main__":
-    map = Map.load_text("maps/01.txt")
+    stage = Stage.load_text("stages/01.txt")
     cards = Card.load_dir("cards/*.txt")
     print(cards)
 
     solver = Solver.new()
-    ans = solver.search(map, cards)
+    ans = solver.search(stage, cards)
 
-# 
-# ________ 
-# ________ 
-# ________ 
-# ________ 
-# ________ 
-# ________ 
-# ________ 
 #
-# ________ 
-# _*____*_ 
-# ___*____ 
+# ________
+# ________
+# ________
+# ________
+# ________
+# ________
+# ________
+#
+# ________
+# _*____*_
+# ___*____
 # *****_**
-# _*__*___ 
-# __*_____ 
+# _*__*___
+# __*_____
 # ********
 #
-# ________ 
-# ________ 
-# ________ 
-# _____*__ 
-# _____**_ 
+# ________
+# ________
+# ________
+# _____*__
+# _____**_
 # ********
 # ********
 # ********
-# 
+#
