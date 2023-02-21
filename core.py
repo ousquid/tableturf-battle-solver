b6bcfdcc3dfa099fc66123ccd5ea54111f3fbed6
Zonkyy <zonk.yy30@gmail.com>
2023-02-22 00:19:40 +0900

    WIP: pattern を set に変更する作業を途中まで完了
    Co-authored-by: MKDir43 <MKDir43@users.noreply.github.com>
    Co-authored-by: koiking213 <koiking213@gmail.com>

--- core.py
+++ core.py
@@ -1,35 +1,92 @@
-from typing import List, Tuple, Self
+from typing import List, Tuple, Self, Set
 import os
 import glob
 import copy
 from enum import Enum
-import numpy as np
 from scipy import ndimage
 import colorama  as cr
+import math
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x:int, y:int):
         self.x = x
         self.y = y
 
+    def __eq__(self, rhs:Self) -> bool:
+        return (self.x == rhs.x) and (self.y == rhs.y)
+
+    def __add__(self, rhs:Self) -> Self:
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+class Cell:
+    def __init__(self, point:Point, sp:bool):
+        self.point = point
+        self.sp = sp
+
+    def rot90(self) -> Self:
+        return Cell(Point(self.point.y, -self.point.x), self.sp)
+
+    def offset_y(self, y:int) -> Self:
+        self.point.y += y
+
+    def __eq__(self, rhs:Self) -> bool:
+        return self.point == rhs.point # ignore sp!!
+
+class Pattern:
+    def __init__(self, cells=None):
+        self.cells: Set[Cell] = cells if cells is not None else set()
+
+    def add(self, cell:Cell):
+        self.cells.add(cell)
+
+    def rot90(self) -> Self:
+        new_cells = {cell.rot90() for cell in self.cells}
+        min_y = min(new_cells, key=lambda c: c.point.y)
+        for cell in new_cells:
+            cell.offset_y(-min_y)
+        return Pattern(new_cells)
+
+    def has(self, point:Point):
+        return Cell(point, False) in self.cells
+
+    @property
+    def shape(self) -> Tuple[int, int]:
+        y = max(self.cells, key=lambda c: c.point.y)
+        x = max(self.cells, key=lambda c: c.point.x)
+        return (y, x)
+
+    def __getitem__(self, item):
+        ys, xs = item
+        new_pat = Pattern()
+        for c in self.cells:
+            if ((ys.start <= c.point.y and c.point.y < ys.stop) and
+                (xs.start <= c.point.x and c.point.x < xs.stop)):
+                new_pat.add(c)
+        return new_pat
+
+    def __and__(self, rhs: Self) -> Self:
+        new_cells = self.cells & rhs.cells
+        return Pattern(new_cells)
+
+    def empty(self) -> bool:
+        return len(self.cells) == 0
 
 class Rotation(Enum):
-    TOP = 0
-    RIGHT = 1
-    BOTTOM = 2
-    LEFT = 3
+    RIGHT = 0
+    BOTTOM = 1
+    LEFT = 2
+    TOP = 3
 
     @classmethod
     def get_values(cls):
         return [i.value for i in cls]
-
 
 class Card:
     def __init__(
         self,
         number: int,
         name: str,
-        pattern: np.ndarray,
+        pattern: Set[Cell],
         ink_spaces: int,
         sp_attack_cost: int,
     ):
@@ -46,14 +103,14 @@ class Card:
 
         max_width = max([len(line.rstrip()) for line in lines])
         max_height = len(lines)
-        pattern = np.zeros((max_height, max_width)).astype(np.uint8)
+        pattern = Pattern()
 
         for y, line in enumerate(lines):
             for x, p in enumerate(line):
                 if p == "x":
-                    pattern[y, x] = 1
+                    pattern.add(Cell(Point(x, y), False))
                 elif p == "o":
-                    pattern[y, x] = 2
+                    pattern.add(Cell(Point(x, y), True))
 
         number = int(os.path.splitext(os.path.basename(path))[0])
         name = "Kojake"
@@ -65,27 +122,26 @@ class Card:
     def load_dir(path: str):
         return [Card.load_text(f) for f in glob.glob(path)]
 
-
 class Placement:
     def __init__(self, card: Card, point: Point, rotation: Rotation):
         self.card = card
         self.point = point
         self.rotation = rotation
 
-    def get_pattern(self) -> np.ndarray:
+    def get_pattern(self) -> Pattern:
         pattern = self.card.pattern
         for _ in range(int(self.rotation)):
-            pattern = np.rot90(pattern)
+            pattern = pattern.rot90()
         return pattern
 
     def __repr__(self):
         return f"<Card: {self.card.number}, {self.card.ink_spaces=}, {self.point.x=}, {self.point.y=}>"
 
 class Stage:
-    def __init__(self, number: int, name: str, pattern: np.ndarray):
+    def __init__(self, number: int, name: str, pattern: Pattern):
         self.number = number
         self.name = name
-        self.pattern = pattern
+        self.pattern: Pattern = pattern
         self.init_pattern = pattern.copy()
         self.place_hist = list()
 
@@ -93,15 +149,15 @@ class Stage:
         y, x = self.pattern.shape
         for y in range(y):
             for x in range(x):
-                if not self.pattern[y, x]:
+                if not self.pattern.has(Point(x, y)):
                     yield Point(x, y)
 
-    def get_slice_indices(self, place: Placement, pattern: np.ndarray):
+    def get_slice_indices(self, place: Placement, pattern: Pattern):
         pat_h, pat_w = pattern.shape
         return (place.point.y, place.point.y + pat_h,
                 place.point.x, place.point.x + pat_w)
 
-    def get_slice(self, place: Placement) -> np.ndarray:
+    def get_slice(self, place: Placement) -> Pattern:
         card_pat = place.get_pattern()
         y_start, y_end, x_start, x_end = self.get_slice_indices(place, card_pat)
         return self.pattern[y_start:y_end, x_start:x_end]
@@ -116,19 +172,20 @@ class Stage:
         # 他のカードと重ならないか
         stage_pat = self.get_slice(place)
 
-        if np.any((card_pat * stage_pat) > 0):
+        if len(card_pat & stage_pat) > 0:
             return False
         return True
 
     def neighbor_pattern(self, place: Placement) -> bool:
-        nosp_pattern = np.where(self.pattern == 2, 1, self.pattern)
-        kernel = ndimage.generate_binary_structure(2, 2)
-        dilated = ndimage.binary_dilation(nosp_pattern, structure=kernel)
-        neighbor = dilated - self.pattern
-        neighbor_stage = Stage(0, "", neighbor)
-        stage_pat = neighbor_stage.get_slice(place)
+        expand_pat = Pattern()
+        for c in self.pattern.cells:
+            for ox in [-1, 0, 1]:
+                for oy in [-1, 0, 1]:
+                    offset = Point(ox, oy)
+                    expand_pat.add(Cell(c.point + offset))
+
         card_pat = place.get_pattern()
-        return np.any((card_pat * stage_pat) > 0)
+        return len((card_pat & expand_pat) > 0)
 
     def put_card(self, place: Placement) -> Self:
         card_pat = place.get_pattern()
